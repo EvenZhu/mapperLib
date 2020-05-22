5f9c43d53301f8c134a9c6a0f95adbd17bc9a45f
Even <even.199010@gmail.com>
2020-05-22 15:12:47 +0800

    init

---
+++ Example/podTestLibrary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  podTestLibrary
+//
+//  Created by even.199010@gmail.com on 05/22/2020.
+//  Copyright (c) 2020 even.199010@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "ZHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZHAppDelegate class]));
+    }
+}
